c27d6ace49ac1bad113a15904aaa827c3fab432d
Neeraj singh <140439504+neeraj-singhh@users.noreply.github.com>
2025-12-29 23:32:53 +0530

    Time: 189 ms (63.06%), Space: 46.7 MB (75.8%) - LeetHub

--- 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.java
+++ 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.java
@@ -1,32 +1,33 @@
 import java.util.*;
 
 class Solution {
-    Map<String, List<Character>> map = new HashMap<>();
-
-    public boolean pyramidTransition(String bottom, List<String> allowed) {
-        for (String s : allowed) {
-            String key = s.substring(0, 2);
-            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s.charAt(2));
-        }
-        return dfs(bottom);
+    int[][] trans = new int[7][7];
+    boolean dfs(int[] row, int len) {
+        if (len == 1) return true;
+        int[] next = new int[len - 1];
+        return build(row, next, 0, len);
     }
-
-    private boolean dfs(String cur) {
-        if (cur.length() == 1) return true;
-        return build(cur, 0, new StringBuilder());
-    }
-
-    private boolean build(String cur, int idx, StringBuilder next) {
-        if (idx == cur.length() - 1) return dfs(next.toString());
-
-        String key = cur.substring(idx, idx + 2);
-        if (!map.containsKey(key)) return false;
-
-        for (char c : map.get(key)) {
-            next.append(c);
-            if (build(cur, idx + 1, next)) return true;
-            next.deleteCharAt(next.length() - 1);
+    boolean build(int[] row, int[] next, int idx, int len) {
+        if (idx == len - 1) return dfs(next, len - 1);
+        int mask = trans[row[idx]][row[idx + 1]];
+        while (mask != 0) {
+            int bit = mask & -mask;
+            next[idx] = Integer.numberOfTrailingZeros(bit);
+            if (build(row, next, idx + 1, len)) return true;
+            mask -= bit;
         }
         return false;
+    }
+    public boolean pyramidTransition(String bottom, List<String> allowed) {
+        for (String s : allowed) {
+            int a = s.charAt(0) - 'A';
+            int b = s.charAt(1) - 'A';
+            int c = s.charAt(2) - 'A';
+            trans[a][b] |= 1 << c;
+        }
+        int n = bottom.length();
+        int[] row = new int[n];
+        for (int i = 0; i < n; i++) row[i] = bottom.charAt(i) - 'A';
+        return dfs(row, n);
     }
 }
